0a8f8da0fe4b5edd8892b8997bcd1c5b713b8b52
Vithusha Ravirajan <vithusharavirajan@Vithushas-MacBook-Pro.local>
2022-11-22 16:38:37 -0500

    completed assignment

---
+++ DNU_get_characters.py
@@ -0,0 +1,18 @@
+# import requests
+# import json
+
+'''
+This is legacy code used to get the list of characters
+'''
+
+# for x in range(1, 17):
+#     response_API = requests.get(f"https://swapi.py4e.com/api/people/{x}")
+#     data = response_API.text
+#     parse_json = json.loads(data)
+#     print(f"{x}:'{parse_json['name']}',")
+
+# for x in range(18, 88):
+#     response_API = requests.get(f"https://swapi.py4e.com/api/people/{x}")
+#     data = response_API.text
+#     parse_json = json.loads(data)
+#     print(f"{x}:'{parse_json['name']}',")
